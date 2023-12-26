1d768376f7e1bf5a56d376cf2cb8190ef83c7f65
buixuanbach1512 <121512997+buixuanbach1512@users.noreply.github.com>
2023-12-26 21:24:48 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,6 +24,13 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(cors({origin: ["https://test-vercel-ivory-mu.vercel.app"], credentials: true}));
 app.use(cookieParser());
 app.use(morgan('dev'));
