64033aa2206cb878cdb09f7d830c8c082078f65c
buixuanbach1512 <121512997+buixuanbach1512@users.noreply.github.com>
2023-12-26 18:28:43 +0700

    Update categoryService.js

--- frontend/src/features/category/categoryService.js
+++ frontend/src/features/category/categoryService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.baseURL = 'https://test-vercel-api-sigma.vercel.app/api/';
 const getAllCategory = async () => {
     const response = await axios.get(`category/`);
     if (response.data) {
