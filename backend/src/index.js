41c8244bcd463497749da446db684cfc31eca551
buixuanbach1512 <121512997+buixuanbach1512@users.noreply.github.com>
2023-12-26 21:05:21 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: ["https://test-vercel-ivory-mu.vercel.app"]}));
+app.use(cors({origin: ["https://test-vercel-ivory-mu.vercel.app"] ,credentials: true}));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
